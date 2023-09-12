0bc61fc0a23b7645ad13d216fbbfc57b1183b883
fikri <fikribakhtiar26@gmail.com>
2023-09-12 15:33:10 +0700

    Update Sourcecode

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,15 +1,17 @@
-class DataSource{
+import clubs from './clubs.js';
+
+class DataSource {
   static searchClub(keyword) {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-  
+
       if (filteredClubs.length) {
         resolve(filteredClubs);
       } else {
         reject(`${keyword} is not found`);
       }
     });
-  }  
+  }
 }
 
-
+export default DataSource;
